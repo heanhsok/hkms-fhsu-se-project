7d215191f8f1dbaee5e44e882c2c58b6ab97aec1
Heanh Sok <heanhsok@gmail.com>
2018-10-13 17:54:17 +0700

    add opportunity page and opportunity page post

---
+++ database/migrations/2018_10_13_103429_create_opportunity_page_posts_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateOpportunityPagePostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('opportunity_page_posts', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('opportunity_page_id')->unsigned();
+            $table->integer('user_id')->unsigned();
+            $table->string('title');
+            $table->string('institution');
+            $table->string('category');
+            $table->string('picture');
+            $table->text('description');
+            $table->text('requirement');
+            $table->text('contact');
+            $table->string('location');
+            $table->date('start_date')->nullable();
+            $table->date('end_date')->nullable();
+            $table->boolean('status')->default(TRUE);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('opportunity_page_posts');
+    }
+}
