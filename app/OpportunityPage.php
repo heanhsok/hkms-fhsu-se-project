fd51b76a6b72122b92074fa66c0d5fa35861fa54
Heanh Sok <heanhsok@gmail.com>
2018-10-13 18:10:14 +0700

    modify opportunity page and opportunity page model

--- app/OpportunityPage.php
+++ app/OpportunityPage.php
@@ -7,4 +7,16 @@ use Illuminate\Database\Eloquent\Model;
 class OpportunityPage extends Model
 {
     //
+    protected $fillable = [
+        'title',
+        'type',
+        'desc',
+        'status'
+    ];
+
+    public function post() {
+        return $this->hasMany('App\OpportunityPagePost');
+    }
+
+
 }
--- app/OpportunityPagePost.php
+++ app/OpportunityPagePost.php
@@ -7,4 +7,23 @@ use Illuminate\Database\Eloquent\Model;
 class OpportunityPagePost extends Model
 {
     //
+    protected $fillable = [
+        'opportunity_page_id',
+        'user_id',
+        'title',
+        'institution',
+        'category',
+        'picture',
+        'description',
+        'requirement',
+        'contact',
+        'location',
+        'start_date',
+        'end_date',
+        'status'
+    ];
+
+    public function opportunity_page() {
+        return $this->belongsTo('App\OpportunityPage');
+    }
 }
